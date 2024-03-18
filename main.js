de7b06df0dd14ac5b18b446d6b85c3e1644b8d38
Iram.Ashfaq <161501174+iram-ashfaq@users.noreply.github.com>
2024-03-18 21:39:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// Question 04 Find a quote from a famous person you admire. Print the quote and the name of its author.
+//Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said,  who never made“A person a mistake never tried anything new.
+var myfavourvitequote = " who never made“A person a mistake never tried anything new.";
+var author = "albert Einstein";
+console.log(myfavourvitequote, author);
+//Question 05 Repeat Exercise 4, but this time store the famous person’s name in a variable called
+//famous_person. Then compose your message and store it in a new variable called message. Print your
+//message
+var famousperson = "RALPH WALDO EMERSON";
+var message = "life is a journery not a destination ";
+console.log(famousperson, message);
+// Question 06 Store a person’s name, and include some whitespace characters at the beginning and end of
+//the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name
+// for \t
+var personname = "\tiram\t";
+console.log(personname);
+// for /n
+var myname = "\niram\n";
+console.log(myname);
